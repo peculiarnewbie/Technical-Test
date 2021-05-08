214c837ce785d42d7441a473d208be9a5b2c8e7d
peculiarnewbie <curiousarmand@gmail.com>
2021-05-09 05:50:38 +0700

    Vuforia working, Blink and Animate
    Also have function to stop the blink and animate effect.

---
+++ Scripts/ModelEffects.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelEffects : MonoBehaviour
+{
+    Material m_Material;
+    public bool isBlinking = true;
+    public bool isAnimating = true;
+    [SerializeField] private float blinkSpeed = 5f;
+    float colorAlpha;
+    float timeSpent = 0;
+    public Animator modelAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Material = GetComponent<Renderer>().material;
+        ModelInteraction.current.OnInteraction += PauseEffects;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSpent += Time.deltaTime * blinkSpeed;
+        if (isBlinking)
+            colorAlpha = (Mathf.Cos(timeSpent) * 3 / 4 + 0.75f);
+        else
+            colorAlpha = 1f;
+
+        m_Material.color = new Color(0.01f, 0.01f, 0.01f, colorAlpha);
+
+        if (isAnimating)
+            modelAnimator.speed = 1;
+        else
+            modelAnimator.speed = 0;
+    }
+
+    //pauses animation and blinking
+    public void PauseEffects()
+    {
+        isBlinking = false;
+        isAnimating = false;
+    }
+
+    public void ResumeEffects()
+    {
+        isBlinking = true;
+        isAnimating = true;
+    }
+}
---
+++ Scripts/ModelInteraction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelInteraction : MonoBehaviour
+{
+    public static ModelInteraction current;
+    [SerializeField] private float swipeRotationSpeed = 5f;
+    [SerializeField] private float scaleSpeed = 5f;
+    Transform modelTransform;
+    float modelScale = 1f;
+    public event Action OnInteraction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = this;
+        modelTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScaleModel();
+    }
+
+    public void ScaleModel()
+    {
+        modelTransform.transform.localScale = Vector3.one * modelScale;
+    }
+
+    public void InteractionFunction()
+    {
+        OnInteraction?.Invoke();
+    }
+}
